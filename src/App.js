b505198d6bd6ed4dc8c1dd39eb59b1fb21b90d48
Brad Bergstrum <bbergstrum@hotmail.com>
2020-12-20 11:50:54 +1100

    refactored handling dynamic changes

--- src/App.js
+++ src/App.js
@@ -51,6 +51,26 @@ class App extends Component {
       cursor: 'pointer',
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -58,25 +78,9 @@ class App extends Component {
         <button
           style={style} 
           onClick={this.togglePersonsHandler}>Switch Name</button>
-        { 
-          this.state.showPersons ? // bellow jsx converted to html behind the scenes by react.createElement()
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Max!')}
-                changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age} />
-            </div> : null // render nothing if showPersons false
-        }
+        { persons }
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
